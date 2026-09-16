54d0e15ab5e63d21f119b23d3016c694f289a382
Pyre <89794440+Fuefos@users.noreply.github.com>
2026-09-16 16:04:29 +0100

    Add files via upload

--- assets/tv/scripts.js
+++ assets/tv/scripts.js
@@ -1,18 +1,27 @@
 let slideIndex = 1;
+
+let slides = document.getElementsByClassName("videoslide");
+let slidecont = []
+for (j = 0; j < slides.length; j++){
+	slidecont.push(slides[j].innerHTML)
+}
 showSlides(slideIndex);
+//console.log(slidecont)
+
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("videoslide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "flex";
+	let i;
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+		slides[i].innerHTML = ""
+	}
+	slides[slideIndex-1].innerHTML = slidecont[slideIndex-1]
+	slides[slideIndex-1].style.display = "flex";
 }
