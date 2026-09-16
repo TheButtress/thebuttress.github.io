45e8c85c8d410a73f3c69e2c8af271fc1a330a4f
Pyre <89794440+Fuefos@users.noreply.github.com>
2026-09-16 15:03:39 +0100

    Add files via upload

--- assets/tv/scripts.js
+++ assets/tv/scripts.js
@@ -14,5 +14,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
 }
