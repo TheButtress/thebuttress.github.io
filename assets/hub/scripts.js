6131010ad9e9c1889e2c64c8edeee949ca4b3171
Pyre <89794440+Fuefos@users.noreply.github.com>
2026-09-19 08:52:09 +0100

    Update scripts.js

--- assets/hub/scripts.js
+++ assets/hub/scripts.js
@@ -1,1 +1,9 @@
-.
+let audioPlayer = document.getElementById('audioPlayer');
+let root = document.querySelector(':root');
+
+function yellowpaint(){
+	root.style.setProperty('--paint', "sepia(1) hue-rotate(13deg) saturate(8) drop-shadow(0px 0px 20px white)");
+    audioPlayer.load();
+	audioPlayer.volume = 0.5;
+    audioPlayer.play();
+}
