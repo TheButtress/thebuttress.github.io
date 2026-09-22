a4dfefe51175c9b1cdcf4d8d20ecc18b6f21adfa
Pyre <89794440+Fuefos@users.noreply.github.com>
2026-09-22 20:34:27 +0100

    Add files via upload

--- assets/drawers/scripts.js
+++ assets/drawers/scripts.js
@@ -94,7 +94,88 @@ function captionset(ss){
 
 
 let slidematrix = [
-	["Autistic Monkee",[["<img class='dispimg' src='./assets/drawers/images/AutisticMonkee/0.webp'</img>",""]]],["Daanootje",[["<img class='dispimg' src='./assets/drawers/images/Daanootje/0.webp'</img>",""]]],["Desi Art",[["<img class='dispimg' src='./assets/drawers/images/DesiArt/0.webp'</img>",""]]],["Devops",[["<img class='dispimg' src='./assets/drawers/images/Devops/0.webp'</img>","Deadlock - before the ritual"]]],["Garghoulie Army",[["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/0.webp'</img>","By Prox"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/1.webp'</img>","By Sol"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/2.webp'</img>","By Vo1d"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/3.webp'</img>","By Vulcan"]]],["Gibberish03",[["<img class='dispimg' src='./assets/drawers/images/Gibberish03/0.webp'</img>",""]]],["GlarnBoudin, Commisioned from Phoenix-Fightmaster",[["<img class='dispimg' src='./assets/drawers/images/GlarnBoudin/0.webp'</img>",""]]],["Kapotalius",[["<img class='dispimg' src='./assets/drawers/images/Kapotalius/0.webp'</img>",""]]],["Leucorrupt",[["<img class='dispimg' src='./assets/drawers/images/Leucorrupt/0.webp'</img>",""]]],["Liv [54BPM]",[["<img class='dispimg' src='./assets/drawers/images/Liv54BPM/0.webp'</img>",""]]],["Madwick",[["<img class='dispimg' src='./assets/drawers/images/Madwick/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Madwick/1.webp'</img>",""]]],["MrCoffeeCobra",[["<img class='dispimg' src='./assets/drawers/images/MrCoffeeCobra/0.webp'</img>",""]]],["Pure Energy",[["<img class='dispimg' src='./assets/drawers/images/PureEnergy/0.webp'</img>",""]]],["Scayris",[["<img class='dispimg' src='./assets/drawers/images/Scayris/0.webp'</img>",""]]],["Sunde3w",[["<img class='dispimg' src='./assets/drawers/images/Sund3w/0.webp'</img>",""]]],["Superspinch & Lopol",[["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/1.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/2.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/3.webp'</img>",""]]],["Vo1d",[["<img class='dispimg' src='./assets/drawers/images/Vo1d/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Vo1d/1.webp'</img>",""]]],["Yomi, Commisioned from nogame_kun",[["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/1.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/2.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/3.webp'</img>",""]]],["Sol",[["<img class='dispimg' src='./assets/drawers/images/sol/0.webp'</img>","This project started 4 days after your debut."],["<img class='dispimg' src='./assets/drawers/images/sol/1.webp'</img>","Final Ghoulie :D"],["<img class='dispimg' src='./assets/drawers/images/sol/2.webp'</img>","Cement tests."],["<img class='dispimg' src='./assets/drawers/images/sol/3.webp'</img>","The family of mould masters and print tests."],["<img class='dispimg' src='./assets/drawers/images/sol/4.webp'</img>","He trapped."],["<img class='dispimg' src='./assets/drawers/images/sol/5.webp'</img>","A lot of failed tests."],["<img class='dispimg' src='./assets/drawers/images/sol/6.webp'</img>","Silicone pour, I had to make a foundry style pouring pot because this took 30 minutes to pour."],["<img class='dispimg' src='./assets/drawers/images/sol/7.webp'</img>","Greased up and ready."],["<img class='dispimg' src='./assets/drawers/images/sol/8.webp'</img>","Time for part 2."],["<img class='dispimg' src='./assets/drawers/images/sol/9.webp'</img>","The mould."],["<img class='dispimg' src='./assets/drawers/images/sol/10.webp'</img>","Bath time :D"],["<img class='dispimg' src='./assets/drawers/images/sol/11.webp'</img>","He is free."],["<img class='dispimg' src='./assets/drawers/images/sol/12.webp'</img>","Looking rough."],["<img class='dispimg' src='./assets/drawers/images/sol/13.webp'</img>","Nice and smooth :)"]]]
+	["Autistic Monkee",[["<img class='dispimg' src='./assets/drawers/images/AutisticMonkee/0.webp'</img>",""]]],
+
+
+
+
+
+	["Danootje",[["<img class='dispimg' src='./assets/drawers/images/Daanootje/0.webp'</img>",""]]],
+
+
+
+
+
+
+
+	["Desi Art",[["<img class='dispimg' src='./assets/drawers/images/DesiArt/0.webp'</img>",""]]],
+
+
+
+
+
+
+
+
+	["Devops",[["<img class='dispimg' src='./assets/drawers/images/Devops/0.webp'</img>","Deadlock - before the ritual"]]],
+
+
+
+
+
+
+
+	["Garghoulie Army",[["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/0.webp'</img>","By Prox"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/1.webp'</img>","By Sol"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/2.webp'</img>","By Vo1d"],["<img class='dispimg' src='./assets/drawers/images/gargouliearmyD/3.webp'</img>","By Vulcan"]]],
+
+
+	["Gibberish03",[["<img class='dispimg' src='./assets/drawers/images/Gibberish03/0.webp'</img>",""]]],
+
+
+
+
+	["GlarnBoudin, Commisioned from Phoenix-Fightmaster",[["<img class='dispimg' src='./assets/drawers/images/GlarnBoudin/0.webp'</img>",""]]],
+
+
+	["Kapotalius",[["<img class='dispimg' src='./assets/drawers/images/Kapotalius/0.webp'</img>",""]]],
+	["Leucorrupt",[["<img class='dispimg' src='./assets/drawers/images/Leucorrupt/0.webp'</img>",""]]],
+	["Liv [54BPM]",[["<img class='dispimg' src='./assets/drawers/images/Liv54BPM/0.webp'</img>",""]]],
+
+
+
+
+	["Madwick",[["<img class='dispimg' src='./assets/drawers/images/Madwick/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Madwick/1.webp'</img>",""]]],
+
+
+
+	["MrCoffeeCobra",[["<img class='dispimg' src='./assets/drawers/images/MrCoffeeCobra/0.webp'</img>",""]]],
+	["Pure Energy",[["<img class='dispimg' src='./assets/drawers/images/PureEnergy/0.webp'</img>",""]]],
+
+	["Scayris",[["<img class='dispimg' src='./assets/drawers/images/Scayris/0.webp'</img>",""]]],
+
+
+
+
+	["Sunde3w",[["<img class='dispimg' src='./assets/drawers/images/Sund3w/0.webp'</img>",""]]],
+
+
+	["Superspinch & Lopol",[["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/1.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/2.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/SuperspinchLopol/3.webp'</img>",""]]],
+
+
+
+
+	["Vo1d",[["<img class='dispimg' src='./assets/drawers/images/Vo1d/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Vo1d/1.webp'</img>",""]]],
+
+
+
+
+
+
+
+
+
+	["Yomi, Commisioned from nogame_kun",[["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/0.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/1.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/2.webp'</img>",""],["<img class='dispimg' src='./assets/drawers/images/Yomiartartistnogamekun/3.webp'</img>",""]]],
+
+	["Sol",[["<img class='dispimg' src='./assets/drawers/images/sol/0.webp'</img>","This project started 4 days after your debut."],["<img class='dispimg' src='./assets/drawers/images/sol/1.webp'</img>","Final Ghoulie :D"],["<img class='dispimg' src='./assets/drawers/images/sol/2.webp'</img>","Cement tests."],["<img class='dispimg' src='./assets/drawers/images/sol/3.webp'</img>","The family of mould masters and print tests."],["<img class='dispimg' src='./assets/drawers/images/sol/4.webp'</img>","He trapped."],["<img class='dispimg' src='./assets/drawers/images/sol/5.webp'</img>","A lot of failed tests."],["<img class='dispimg' src='./assets/drawers/images/sol/6.webp'</img>","Silicone pour, I had to make a foundry style pouring pot because this took 30 minutes to pour."],["<img class='dispimg' src='./assets/drawers/images/sol/7.webp'</img>","Greased up and ready."],["<img class='dispimg' src='./assets/drawers/images/sol/8.webp'</img>","Time for part 2."],["<img class='dispimg' src='./assets/drawers/images/sol/9.webp'</img>","The mould."],["<img class='dispimg' src='./assets/drawers/images/sol/10.webp'</img>","Bath time :D"],["<img class='dispimg' src='./assets/drawers/images/sol/11.webp'</img>","He is free."],["<img class='dispimg' src='./assets/drawers/images/sol/12.webp'</img>","Looking rough."],["<img class='dispimg' src='./assets/drawers/images/sol/13.webp'</img>","Nice and smooth :)"]]]
 
 
 	
